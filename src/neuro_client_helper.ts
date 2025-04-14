6526225cfd6480d5645503535b326c4965d9d4b9
Pasu4 <pascalwieckhorst02@gmail.com>
2025-04-14 20:00:39 +0200

    Add more helpers

--- src/neuro_client_helper.ts
+++ src/neuro_client_helper.ts
@@ -34,6 +34,23 @@ export function actionResultAccept(message?: string): ActionResult {
 }
 
 /**
+ * Create an action result with the specified message.
+ * This should be used if the action failed, but should not be retried, e.g.
+ * if the source of the error is out of Neuro's control or to prevent a retry
+ * loop in case the action is not applicable in the current state.
+ * @param message The message to send to Neuro.
+ * This should explain, if possible, why the action failed.
+ * If omitted, will just send "Action failed.".
+ * @returns A successful action result with the specified message.
+ */
+export function actionResultFailure(message?: string): ActionResult {
+    return {
+        success: true,
+        message: message !== undefined ? `Action failed: ${message}` : 'Action failed.',
+    };
+}
+
+/**
  * Create an action result that tells Neuro to retry the forced action.
  * @param message The message to send to Neuro.
  * This should contain the information required to fix the mistake.
@@ -42,7 +59,7 @@ export function actionResultAccept(message?: string): ActionResult {
 export function actionResultRetry(message: string): ActionResult {
     return {
         success: false,
-        message: message,
+        message: `Action failed: ${message}`,
     };
 }
 
@@ -54,6 +71,36 @@ export function actionResultRetry(message: string): ActionResult {
 export function actionResultMissingParameter(parameterName: string) {
     return {
         success: false,
-        message: `Missing required parameter "${parameterName}"`,
+        message: `Action failed: Missing required parameter "${parameterName}"`,
     };
 }
+
+/**
+ * Create an action result that tells Neuro that she doesn't have the required permission.
+ * @param permission The permission.
+ * Use a string from {@link PERMISSION_STRINGS} if possible,
+ * otherwise it should fit in the sentence "You do not have permission to {permission}.".
+ * @returns A successful action result with a message pointing out the missing permission.
+ */
+export function actionResultNoPermission(permission: string) {
+    return {
+        success: true,
+        message: `Action failed: You do not have permission to ${permission}.`
+    };
+}
+
+/** Collection of strings for use in {@link actionResultNoPermission}. */
+export const PERMISSION_STRINGS = {
+    openFiles:          'open files',
+    editActiveDocument: 'edit the current document',
+    create:             'create files or folders',
+    rename:             'rename files or folders',
+    delete:             'delete files or folders',
+    runTasks:           'run tasks',
+    requestCookies:     'request cookies',
+    gitOperations:      'use Git',
+    gitTags:            'tag commits',
+    gitRemotes:         'interact with Git remotes',
+    editRemoteData:     'edit remote data',
+    gitConfigs:         'edit the Git configuration',
+};
