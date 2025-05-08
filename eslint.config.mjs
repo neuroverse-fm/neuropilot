fe56102e9d81550dee23159da0054fd05542763c
Pasu4 <pascalwieckhorst02@gmail.com>
2025-05-08 11:48:13 +0200

    Accidentaly ignored instead of fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-/* eslint-disable @stylistic/quotes */
-/* eslint-disable @stylistic/indent */
 /**
  * ESLint configuration for the project.
  * 
@@ -11,45 +9,45 @@ import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config(
-	{
-		files: ['**/*.{js,mjs,cjs,ts}'],
-		ignores: [
-			'out',
-			'playground/**',
-			'**/vscode*.d.ts',
-		],
-	},
-	js.configs.recommended,
-	...tseslint.configs.recommended,
-	...tseslint.configs.stylistic,
-	{
-		plugins: {
-			'@stylistic': stylistic,
-		},
-		rules: {
-			'curly': 'off',
-			'no-control-regex': 'off',
-			'@stylistic/semi': ['warn', 'always'],
-			'@stylistic/indent': ['error', 4],
-			'@stylistic/comma-dangle': ['warn', 'always-multiline'],
-			'@stylistic/eol-last': ["warn", "always"],
-			'@stylistic/no-extra-parens': ["warn", "all"],
-			'@stylistic/no-trailing-spaces': ['warn', { 'ignoreComments': true }],
-			'@stylistic/quotes': ['error', 'single', { 'avoidEscape': true }],
-			'@typescript-eslint/no-empty-function': 'off',
-			'@typescript-eslint/naming-convention': [
-				'warn',
-				{
-					'selector': 'import',
-					'format': ['camelCase', 'PascalCase'],
-				},
-			],
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					'argsIgnorePattern': '^_',
-				},
-			],
-		},
-	},
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        ignores: [
+            'out',
+            'playground/**',
+            '**/vscode*.d.ts',
+        ],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            'curly': 'off',
+            'no-control-regex': 'off',
+            '@stylistic/semi': ['warn', 'always'],
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+            '@stylistic/eol-last': ['warn', 'always'],
+            '@stylistic/no-extra-parens': ['warn', 'all'],
+            '@stylistic/no-trailing-spaces': ['warn', { 'ignoreComments': true }],
+            '@stylistic/quotes': ['error', 'single', { 'avoidEscape': true }],
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    'selector': 'import',
+                    'format': ['camelCase', 'PascalCase'],
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    'argsIgnorePattern': '^_',
+                },
+            ],
+        },
+    },
 );
