ccead89d483ad06ac02d4ff3727a8a7d788dd870
KTrain5369 <git@ktrain5369.me>
2025-06-20 20:13:23 +1000

    switch to esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const esbuild = require('esbuild');
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+async function main() {
+    const ctx = await esbuild.context({
+        entryPoints: ['src/web/extension.ts', 'src/web/test/suite/extensionTests.ts'],
+        bundle: true,
+        format: 'cjs',
+        minify: production,
+        sourcemap: !production,
+        sourcesContent: false,
+        platform: 'browser',
+        outdir: 'dist/web',
+        external: ['vscode'],
+        logLevel: 'warning',
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis',
+        },
+    });
+    if (watch) {
+        await ctx.watch();
+    } else {
+        await ctx.rebuild();
+        await ctx.dispose();
+    }
+}
+
+main().catch(erm => {
+    console.error(erm);
+    process.exit(1);
+});
