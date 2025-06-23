124457fd6827a5dc61a1c125bf29fe4ca4851bbb
Pasu4 <117922538+Pasu4@users.noreply.github.com>
2025-06-23 12:43:40 +0200

    Update .vscode-test.mjs
    Co-authored-by: KTrain <69028025+KTrain5169@users.noreply.github.com>

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
+    label: 'desktopUnitTest'
     files: 'out/test/**/*.test.js',
     workspaceFolder: './test-playground',
 });
