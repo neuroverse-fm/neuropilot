b11ae8287f2001a42c2fd9a8528ea63fa22a42b5
KTrain5369 <git@ktrain5369.me>
2025-07-17 12:43:11 +1000

    platform option for consistency with web

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from '@vscode/test-cli';
 export default defineConfig([
     {
         label: 'desktopUnitTest',
+        platform: 'desktop',
         files: 'out/test/**/*.test.js',
         workspaceFolder: './test-playground',
+        browser: '',
         coverage: {
             include: ['src/**/*.ts'],
             exclude: ['src/test/**/*.test.ts', 'src/test/suite/index.ts'],
