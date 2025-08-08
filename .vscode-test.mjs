00b3c4b5e3b19a072a22651b1fe957922b9e74b1
KTrain5369 <me@git.ktrain5369.dev>
2025-08-08 10:01:59 +1000

    config changes for tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -4,12 +4,12 @@ export default defineConfig([
     {
         label: 'desktopUnitTest',
         platform: 'desktop',
-        files: 'out/test/**/*.test.js',
+        files: 'out/test/desktop/**/*.test.js',
         workspaceFolder: './test-playground',
         browser: '',
         coverage: {
             include: ['src/**/*.ts'],
-            exclude: ['src/test/**/*.test.ts', 'src/test/suite/index.ts'],
+            exclude: ['src/test/**/*.test.ts', 'src/test/suite/index.ts', 'src/web/**/*'],
             output: './coverage',
         },
         env: {
