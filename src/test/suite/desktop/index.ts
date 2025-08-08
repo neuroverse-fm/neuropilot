8a3c6bc381dcb6b02e11d2e360689c6edec2e466
KTrain5369 <me@git.ktrain5369.dev>
2025-08-08 16:52:27 +1000

    move index.ts + extension.test.ts to desktop folder inside test suite

--- src/test/suite/desktop/index.ts
+++ src/test/suite/desktop/index.ts
@@ -12,7 +12,10 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot })
+        glob('**/**.test.js', {
+            cwd: testsRoot,
+            ignore: ['web/**.test.js'],
+        })
             .then(files => {
                 // Add files to the test suite
                 files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
