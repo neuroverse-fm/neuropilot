3f9cbee505dea592f50e5dc636fdedae42ab9eb2
KTrain5369 <me@git.ktrain5369.dev>
2025-09-12 20:36:39 +1000

    reload permissions on permission change automatically, properly block workspace-level config changes to terminal access permission

--- src/shared/extension.ts
+++ src/shared/extension.ts
@@ -69,6 +69,9 @@ export function setupCommonEventHandlers() {
             ) {
                 setVirtualCursor();
             }
+            if (event.affectsConfiguration('neuropilot.permission')) {
+                reloadPermissions();
+            }
         }),
         vscode.extensions.onDidChange(obtainExtensionState),
     ];
