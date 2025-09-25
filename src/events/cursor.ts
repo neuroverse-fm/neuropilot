e4b3110bc701b996d069ccfe01cae3c3f49e2c6e
KTrain5369 <me@git.ktrain5369.dev>
2025-09-25 15:11:18 +1000

    minor indenting issue that annoys me

--- src/events/cursor.ts
+++ src/events/cursor.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 /** Cursor moved event - fires when Neuro's cursor is moved for any reason
  * Null typing is used if the cursor is not on the page.
  * Undefined is used when the text editor does not exist.
-*/
+ */
 const _onDidMoveCursor = new vscode.EventEmitter<vscode.Position | null | undefined>();
 
 /** 
