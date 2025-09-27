430c2c33a57e56f5a52474d70d0def302444b1ad
KTrain <69028025+KTrain5169@users.noreply.github.com>
2025-09-27 11:05:27 +1000

    Update src/events/shells.ts
    Co-authored-by: Pasu4 <117922538+Pasu4@users.noreply.github.com>

--- src/events/shells.ts
+++ src/events/shells.ts
@@ -2,8 +2,9 @@ import * as vscode from 'vscode';
 import { RCECancelEvent } from './utils';
 import { NEURO } from '../constants';
 
-export function notifyOnTerminalClose(terminal: string): RCECancelEvent {
+export function notifyOnTerminalClose(terminal: string): RCECancelEvent | null {
     const shell = NEURO.terminalRegistry.get(terminal);
+    if (!shell) return null;
     const event = new RCECancelEvent({
         reason: `the terminal ${terminal} was closed.`,
     });
