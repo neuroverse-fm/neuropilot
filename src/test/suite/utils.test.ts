38266c18a0d7af6a6d6b7573878983d608a152d5
KTrain5369 <me@git.ktrain5369.dev>
2026-02-06 22:09:29 +1000

    oops

--- src/test/suite/utils.test.ts
+++ src/test/suite/utils.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import * as utils from '../@/utils/misc';
+import * as utils from '@/utils/misc';
 import { NEURO } from '../../constants';
 import { anyString, capture, reset, spy, verify } from 'ts-mockito';
 
